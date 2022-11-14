f38fbe1c01e3de821ed0c299a38e00732d8822b8
Dustin Hays <hays.dustin.u@gmail.com>
2022-11-14 04:51:38 -0800

    commit

--- js/main.js
+++ js/main.js
@@ -11,26 +11,26 @@
 // }
 // console.log(findOddInt(arr))
 
-obj = {
-    first : '1',
-    second : '2',
-    third : false,
-    fourth : ['anytime', 2, 3, 4],
-    fifth : null
-}
+// obj = {
+//     first : '1',
+//     second : '2',
+//     third : false,
+//     fourth : ['anytime', 2, 3, 4],
+//     fifth : null
+// }
 
-function strCount(obj) {
-    // count all string values inside of an object.
-    let value = 0
-    for(key in obj){
-        if(typeof obj[key] === 'string'){
-            value ++
-        }
-        if(typeof obj[key] === 'object'){
-          value +=  strCount(obj[key])
-        }
-    }
-    return value
-}
+// function strCount(obj) {
+//     // count all string values inside of an object.
+//     let value = 0
+//     for(key in obj){
+//         if(typeof obj[key] === 'string'){
+//             value ++
+//         }
+//         if(typeof obj[key] === 'object'){
+//           value +=  strCount(obj[key])
+//         }
+//     }
+//     return value
+// }
 
-console.log(strCount(obj))
+// console.log(strCount(obj))
