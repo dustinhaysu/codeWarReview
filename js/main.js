bf6da456e41b9969dafc9a60860ac395665e4e23
Dustin Hays <hays.dustin.u@gmail.com>
2022-11-14 04:13:03 -0800

    test

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// 06FindOddInt
+// let arr = [88,0,0,0,0,2,2]
+// function findOddInt(arr){
+//     let obj = {}
+//     arr.forEach(key => obj[key] ? obj[key]++ : obj[key] = 1 )
+//     for(prop in obj){
+//         if (obj[prop] %2 !== 0){
+//             return Number(prop)
+//         }
+//     }
+// }
+// console.log(findOddInt(arr))
