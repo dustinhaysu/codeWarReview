952570a70eb17abe0c73f69991a6ef17b86d7c59
Dustin Hays <hays.dustin.u@gmail.com>
2022-11-22 07:54:47 -0800

    commit

--- js/main.js
+++ js/main.js
@@ -121,19 +121,19 @@ let discovered = 'tiiesae'
 // console.log(uncensor(infected, discovered))
 
 //**************************************************** */
-//  let stringOne = 'bubble gum'
-//  let stringTwo = 'turtle ham'
+ let stringOne = 'bubble gum'
+ let stringTwo = 'turtle ham'
 
-//  function mutateMyStrings(stringOne, stringTwo){
-//     let arr1 = stringOne.split('')
-//     let arr2 = stringTwo.split('')
-//     let finalValue = [stringOne]
-//     for(i=0; i<arr1.length; i++){
-//         arr1[i] = arr2[i]
-//         finalValue.push(arr1.join(""))
-//     }
-//     return finalValue.join('\n')
-//  }
-//  console.log(mutateMyStrings(stringOne, stringTwo))
+ function mutateMyStrings(stringOne, stringTwo){
+    let arr1 = stringOne.split('')
+    let arr2 = stringTwo.split('')
+    let finalValue = [stringOne]
+    for(i=0; i<arr1.length; i++){
+        arr1[i] = arr2[i]
+        finalValue.push(arr1.join(""))
+    }
+    return finalValue.join('\n')
+ }
+ console.log(mutateMyStrings(stringOne, stringTwo))
 
 //****************************************** */
