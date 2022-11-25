69bfbbf698044953d20fcb28edca83df768da3a8
Dustin Hays <hays.dustin.u@gmail.com>
2022-11-25 07:50:22 -0800

    commit

--- js/main.js
+++ js/main.js
@@ -303,3 +303,27 @@
     //     default: return names[0 +',' + names[1] + 'and' + (names.length -2) +'others like this';      
     //  }
     // }
+
+    //******************************************************************** */
+let str1 = 'abcde'
+let str2 = 'cdefgh'
+let str3 = 'abaab'
+let str4 = 'aabab'
+
+// split strings, count like occurrences, slice array 2
+
+    function mergeStrings(str1, str2){
+       let arr1 = str1.split('')
+        let arr2 = str2.split('')
+        let repeatCount = 1
+
+        for(i=0; i<arr1.length -1; i++){
+            if(arr1[i] === arr2[i] && arr1[i+1] === arr2[i+1]){
+                repeatCount = 1+1
+            }
+        }
+        return arr2.slice(repeatCount).join('')
+
+
+    }
+    console.log(mergeStrings(str1, str2))
