90814d942a83056ea658f9ac5eb24a9db5b1ec62
Dustin Hays <hays.dustin.u@gmail.com>
2022-12-03 03:11:36 -0800

    commit

--- js/main.js
+++ js/main.js
@@ -768,19 +768,21 @@ const arr2 = [78, 42, "ciao"]
 //     return newArr;
 //   }
 
-  function hotSingles(arr1, arr2){
-    var newArr = []
-    for(let i = 0; i<arr1.length;i++){
-        if(!newArr.includes(arr1[i]) && !arr2.includes(arr1[i])){
-            newArr.push(arr1[i])
-        }
-    }
-    for(var i = 0; i< arr2.length; i++){
-        if(!newArr.includes(arr2[i]) && !arr1.includes(arr2[i])){
-            newArr.push(arr2[i])
-        }
-    }
-    return newArr;
-  }
+//   function hotSingles(arr1, arr2){
+//     var newArr = []
+//     for(let i = 0; i<arr1.length;i++){
+//         if(!newArr.includes(arr1[i]) && !arr2.includes(arr1[i])){
+//             newArr.push(arr1[i])
+//         }
+//     }
+//     for(var i = 0; i< arr2.length; i++){
+//         if(!newArr.includes(arr2[i]) && !arr1.includes(arr2[i])){
+//             newArr.push(arr2[i])
+//         }
+//     }
+//     return newArr;
+//   }
 
-  console.log(hotSingles(arr1, arr2))
+//   console.log(hotSingles(arr1, arr2))
+//********************************************************* */
+
