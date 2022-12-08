d75a2ae718b252f2f0aa1867a426c4ab0ed35c3b
Dustin Hays <hays.dustin.u@gmail.com>
2022-12-08 04:02:33 -0800

    commit

--- js/main.js
+++ js/main.js
@@ -951,3 +951,22 @@
 
 //********************************************************************************************** */
 
+// const arrays = [[1,2,3,4,5,6], [7,7,7,-7,7,7], [1,1,1,1,1,1]]
+// let shift = 3
+
+
+// function addingShifted (arrays, shift) {
+//     let result = [];
+//     let start = 0;
+//     for (let array of arrays) {
+//     for (let i = 0; i < array.length; i++) {
+//     result[start + i] = (result[start + i] || 0) + array[i];
+//     }
+//     start += shift;
+//     }
+//     return result;
+//     }
+//     console.log(addingShifted(arrays, shift))
+
+//*************************************************************************** */
+
