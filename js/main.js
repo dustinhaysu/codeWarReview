6d0bbc160f38af1872de0698a15650d27e0aba26
Dustin Hays <hays.dustin.u@gmail.com>
2022-12-16 02:32:52 -0800

    commit

--- js/main.js
+++ js/main.js
@@ -1224,3 +1224,18 @@
 
 //************************************************************ */
 
+// const arr1 = [1, 2, 2, 3, 4]
+// const arr2 = [2, 1, 2, 4, 3]
+// const arr3 = [1, 2, 3, 4]
+// const arr = [1, 2, 3, "4"]
+
+
+// function arraysSimilar(arr1, arr2) {
+//     arr1 = arr1.sort(sortFunc);
+//     arr2 = arr2.sort(sortFunc);
+//     return arr1.length == arr2.length && arr1.every((a,i) => a === arr2[i]);
+//     }
+//     function sortFunc(a,b) {return a-b;}
+//     console.log(arraysSimilar(arr1, arr2))
+//*********************************************************************** */
+
