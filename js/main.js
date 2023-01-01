cd37353dedf95a5ee4fd64efbf7c7cab5241167b
Dustin Hays <hays.dustin.u@gmail.com>
2023-01-01 02:16:02 -0800

    commit

--- js/main.js
+++ js/main.js
@@ -1496,3 +1496,19 @@
 
 //************************************ */
 
+// function multiplesOf3Or5(num){
+//     let sum = 0
+//     for(let i = 0; i < num; i++){
+//         if(i % 3 === 0 && i % 5 === 0){
+//             sum += i           
+//         }
+//        else if(i % 3 === 0 || i % 5 === 0){
+//         sum += i
+//        }
+//     }
+//     return sum
+// }
+// console.log(multiplesOf3Or5(10))
+
+//*********************************** */
+
