f5262f57240cee331edd9224e7ac606c44049877
Dustin Hays <hays.dustin.u@gmail.com>
2023-01-03 05:24:38 -0800

    commit

--- js/main.js
+++ js/main.js
@@ -1545,3 +1545,13 @@
 // console.log(sumArray(arr))
 
 //*********************************** */
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+// function phone(arr){
+// return `(${arr[0]}${arr[1]}${arr[2]}) ${arr[3]}${arr[4]}${arr[5]}-${arr[6]}${arr[7]}${arr[8]}${arr[9]}`}
+
+// console.log(phone(arr))
+
+//************************************** */
+
