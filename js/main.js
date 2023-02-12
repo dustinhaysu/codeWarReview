7adac1180d21c6277e1cf639282edecf84404aaf
Dustin Hays <hays.dustin.u@gmail.com>
2023-02-12 02:17:09 -0800

    commit

--- js/main.js
+++ js/main.js
@@ -1934,3 +1934,21 @@
 
 //************************************ */
 
+// s = "abcd\nefgh\nijkl\nmnop"
+
+// function vertMirror(str){
+//     return str.split('\n').map(line=>line.split('').reverse().join('')).join('\n')
+// }
+
+// function horMirror(str){
+//     return str.split('\n').reverse().join('\n')
+// }
+
+// function oper(fct, s){
+//     return fct(s)
+// }
+
+// console.log(oper(vertMirror, s))
+
+//*************************************** */
+
