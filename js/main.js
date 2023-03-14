909af3e12ac77ec470c7a6863e340ad01679e928
Dustin Hays <hays.dustin.u@gmail.com>
2023-03-14 02:33:14 -0700

    commit

--- js/main.js
+++ js/main.js
@@ -2268,3 +2268,21 @@
 
 //************************************************ */
 
+// function likes (arr) {
+//     switch(arr.length){
+//         case 0 : return `nobody likes this.`;
+//         break;
+//         case 1 : return `${arr[0]} likes this`;
+//         break;
+//         case 2 : return `${arr[0]} and ${arr[1]} likes this.`;
+//         break;
+//         case 3 : return `${arr[0]}, ${arr[1]} and ${arr[2]} likes this`;
+//         break;
+//         default : return `${arr[0]}, ${arr[1]} and ${arr.length - 2} others like this`
+//     }
+// }
+
+// console.log(likes([]))
+
+//******************************************* */
+
